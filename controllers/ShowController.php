8f626aec97eab01b902260e1dd29f47e8c92203a
“canon1014” <717437709@qq.com>
2019-06-01 12:45:52 +0800

    推进

--- controllers/ShowController.php
+++ controllers/ShowController.php
@@ -18,7 +18,7 @@ class ShowController extends Controller
         $page = $_GET['page'];
         if($hyId == 'null')  $hyId = $typeId;
         if(!isset($page))  $page = 1;
-        $data = file_get_contents("http://restapi.amap.com/v3/place/text?parameters&key=f38ab40b387c97c182caa1c4bb96c08e&types={$typeId}&city={$cityId}&offset=25&page={$page}");
+        $data = file_get_contents("http://restapi.amap.com/v3/place/text?parameters&key=f38ab40b387c97c182caa1c4bb96c08e&types={$typeId}&city={$cityId}&offset=50&page={$page}");
         $result = json_decode($data,true);
         return $this->render('index',compact('result'));
     }
--- views/show/index.php
+++ views/show/index.php
@@ -19,28 +19,97 @@ use yii\helpers\Html;
         <table id="table-breakpoint">
             <thead>
             <tr>
-                <th>Name</th>
-                <th>Age</th>
-                <th>Gender</th>
-                <th>Height</th>
-                <th>Province</th>
-                <th>Sport</th>
+                <th>名称</th>
+                <th>所属行业</th>
+                <th>联系电话</th>
+                <th>所在地址</th>
+                <th>图片</th>
             </tr>
             </thead>
             <tbody>
+            <?php
+                foreach($result['pois'] as $key=>$value){
+
+            ?>
+
             <tr>
-                <td data-th="Name"><span class="bt-content">James Camera</span></td>
-                <td data-th="Age"><span class="bt-content">31</span></td>
-                <td data-th="Gender"><span class="bt-content">Male</span></td>
-                <td data-th="Height"><span class="bt-content">6'1</span></td>
-                <td data-th="Province"><span class="bt-content">British Columbia</span></td>
-                <td data-th="Sport"><span class="bt-content">Hiking</span></td>
+                <td data-th="name"><span class="bt-content"><?= $value['name'] ?></span></td>
+                <td data-th="type"><span class="bt-content"><?= $value['type'] ?></span></td>
+                <td data-th="address">
+                    <span class="bt-content">
+                        <?php
+                        if (!is_array($value['address'])){
+                            echo $value['address'];
+                        }else{
+                            if (empty($value['address'])){
+                                echo '';
+                            };
+                        }
+                        ?>
+                    </span></td>
+                <td data-th="Height">
+                    <span class="bt-content">
+                        <?php
+                        if (!is_array($value['tel'])){
+                            echo str_replace(';', "<br/>", $value['tel']);
+                        }else{
+                            if (empty($value['tel'])){
+                                echo '';
+                            };
+                        }
+                        ?>
+                    </span>
+                </td>
+                <td data-th="Height">
+                    <span class="bt-content">
+                        <?php
+                            $photos = [
+                                'title' => $value['name'] ,
+                                'id'    => $value['id'] ,
+                                'start' => 0,
+                                'data'  => []
+                            ];
+                            foreach ($value['photos'] as $k=>$photo){
+                                $photos['data'][] = [
+                                    'alt' => $k,
+                                    'pid' => $k,
+                                    'src' => $photo['url'],
+                                    'thumb' => $photo['url']
+                                ];
+                            }
+                        ?>
+                    </span>
+                </td>
             </tr>
+
+            <?php
+                }
+            ?>
             </tbody>
         </table>
     </div>
 </div>
-
+<div class="jogger">
+    <span class="disabled">&lt; </span>
+    <span class="current">1</span>
+    <a href="#?page=2">2</a>
+    <a href="#?page=3">3</a>
+    <a href="#?page=4">4</a>
+    <a href="#?page=5">5</a>
+    <a href="#?page=6">6</a>
+    <a href="#?page=7">7</a>...
+    <a href="#?page=199">199</a>
+    <a href="#?page=200">200</a>
+    <a href="#?page=2">
+        &gt; </a></div>
 
 </body>
 </html>
+<script src="./static/assets/js/jquery-1.8.3.min.js"></script>
+<?=Html::jsFile('@web/static/layer/layer.js')?>
+<script>
+
+    layer.msg('hello');
+
+
+</script>
